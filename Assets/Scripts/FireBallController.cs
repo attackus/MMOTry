49ae144f9761ad48697a21506d9128be4a161d42
Ethan Currah <ECurrah90@gmail.com>
2015-03-15 21:15:44 -0400

    Added player and fireball attack.

---
+++ Assets/Scripts/FireBallController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBallController : MonoBehaviour {
+
+    public float fireAttackSpeed = 1f;
+
+    public GameObject explosionPrefab;
+
+	// Use this for initialization
+	void Start () {
+        if (explosionPrefab == null)
+        {
+            Debug.Log("ExplodeOnImpact Script does not have an explosion prefab attached. Disabling Script.");
+            this.enabled = false;
+        }
+	}
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * fireAttackSpeed);
+        Destroy(gameObject, 3);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Instantiate(explosionPrefab, transform.position, transform.rotation);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/SpellCast.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellCast : MonoBehaviour {
+
+    public GameObject spellPrefab;
+
+	// Use this for initialization
+	void Start () {
+        if (spellPrefab == null)
+        {
+            Debug.Log("SpellCast script does not have a spell prefab attached. Disabling script.");
+            this.enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Instantiate(spellPrefab, transform.position, transform.rotation);
+        }
+	}
+}
